806a5f358c1678378136d88a37f9b196b0655232
BigDog93 <ljc9393@nate.com>
2021-07-26 11:52:35 +0900

    2021.07.26 boxoffice 출력 예제 완료

--- MVBunker/src/main/java/com/nurim/mvbunker/movies/model/BeanConfig.java
+++ MVBunker/src/main/java/com/nurim/mvbunker/movies/model/BeanConfig.java
@@ -6,22 +6,32 @@ import info.movito.themoviedbapi.TmdbMovies;
 import info.movito.themoviedbapi.model.Genre;
 import info.movito.themoviedbapi.model.core.MovieResultsPage;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-@Component
+@Configuration
 public class BeanConfig {
     String api_key = "6956427bb4a25b1080cb617aa6e2194e";
+
     @Bean
     public TmdbApi getTmdbApi() {
         return new TmdbApi(api_key);
     }
-    @Bean
-    public List<Genre> getGenreList() {
+
+    @Bean(name="genreMap")
+    public Map<Integer, String> getGenreList() {
         TmdbApi tmdbApi = new TmdbApi(api_key);
         List<Genre> genreList = tmdbApi.getGenre().getGenreList("ko-KR");
-        return genreList;
+
+        Map<Integer,String> genreMap = new HashMap<>();
+        for(int i = 0; i < genreList.size(); i++) {
+            genreMap.put(genreList.get(i).getId(), genreList.get(i).getName());
+        }
+        return genreMap;
     }
 
 
--- MVBunker/src/main/java/com/nurim/mvbunker/movies/model/MyMovieDb.java
+++ MVBunker/src/main/java/com/nurim/mvbunker/movies/model/MyMovieDb.java
@@ -1,18 +1,82 @@
 package com.nurim.mvbunker.movies.model;
 
-import info.movito.themoviedbapi.model.MovieDb;
+import info.movito.themoviedbapi.model.*;
+import info.movito.themoviedbapi.model.keywords.Keyword;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MyMovieDb extends MovieDb {
-    @Autowired private GenreLists genreLists;
 
-    private List<String> GenreNames;
+    private List<String> genreNames;
+
+    private List<Artwork> images;
+
+    private List<AlternativeTitle> alternativeTitles;
+
+    private  List<Keyword> keywords;
+
+    private List<ReleaseInfo> releases;
+
+    private List<Video> videos;
+
+    private List<Translation> translations;
+
+    private List<MovieDb> similarMovies;
+
+    private List<MovieDb> recommendedMovies;
+
+    private List<MovieList> lists;
+
+    private List<Reviews> reviews;
+
+    public MyMovieDb(MovieDb origin) {
+        this.setTitle(origin.getTitle());
+        this.setOriginalTitle(origin.getOriginalTitle());
+        this.setPopularity(origin.getPopularity());
+        this.setBackdropPath(origin.getBackdropPath());
+        this.setPosterPath(origin.getPosterPath());
+        this.setReleaseDate(origin.getReleaseDate());
+        this.setAdult(origin.isAdult());
+        this.setBelongsToCollection(origin.getBelongsToCollection());
+        this.setBudget(origin.getBudget());
+        this.setGenres(origin.getGenres());
+        this.setHomepage(origin.getHomepage());
+        this.setOverview(origin.getOverview());
+        this.setImdbID(origin.getImdbID());
+        this.setOriginalLanguage(origin.getOriginalLanguage());
+        this.setProductionCompanies(origin.getProductionCompanies());
+        this.setProductionCountries(origin.getProductionCountries());
+        this.setRevenue(origin.getRevenue());
+        this.setRuntime(origin.getRuntime());
+        this.setSpokenLanguages(origin.getSpokenLanguages());
+        this.setTagline(origin.getTagline());
+        this.setUserRating(origin.getUserRating());
+        this.setVoteAverage(origin.getVoteAverage());
+        this.setVoteCount(origin.getVoteCount());
+        this.setStatus(origin.getStatus());
+        this.setAlternativeTitles(origin.getAlternativeTitles());
+        this.setCredits(origin.getCredits());
+        this.setImages(origin.getImages());
+        this.setKeywords(origin.getKeywords());
+        this.setReleases(origin.getReleases());
+        this.setVideos(origin.getVideos());
+        this.setTranslations(origin.getTranslations());
+        this.setSimilarMovies(origin.getSimilarMovies());
+        this.setRecommendedMovies(origin.getRecommendations());
+        this.setReviews(origin.getReviews());
+        this.setLists(origin.getLists());
+        this.setGenreNames(new ArrayList<>());
+    }
 
     // 오버라이드 해버리기.. API 에서 불러오자마자 걍 넣어버리기...
     /*@Override
