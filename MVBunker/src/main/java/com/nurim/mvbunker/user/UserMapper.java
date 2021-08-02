f3ecfe115d35e15a37a8cefc34035e7f86a0598c
Ey <dmss3@naver.com>
2021-08-02 15:09:28 +0900

    마이페이지 > 내 프로필 > 내 활동내역 (mapper, service, controller)

--- MVBunker/src/main/java/com/nurim/mvbunker/user/UserMapper.java
+++ MVBunker/src/main/java/com/nurim/mvbunker/user/UserMapper.java
@@ -13,4 +13,5 @@ public interface UserMapper {
 
     int countMyReview(UserEntity param);
     int countMyCmt(UserEntity param);
+    int countMyReply(UserEntity param);
 }
---
+++ MVBunker/src/main/java/com/nurim/mvbunker/user/model/Activity.java
@@ -0,0 +1,11 @@
+package com.nurim.mvbunker.user.model;
+
+import lombok.Data;
+
+@Data
+
+public class Activity {
+    private int myReview;
+    private int myReviewCmt;
+    private int myReply;
+}
