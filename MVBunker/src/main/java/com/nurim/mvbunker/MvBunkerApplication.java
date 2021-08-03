09cb2ea8eb626a2498651863be38f0660149d4e0
jennienterry <hello1913@naver.com>
2021-08-03 15:33:47 +0900

    2021.08.03 리뷰 디테일 인피니트 스크롤 작업중

--- MVBunker/src/main/java/com/nurim/mvbunker/MvBunkerApplication.java
+++ MVBunker/src/main/java/com/nurim/mvbunker/MvBunkerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MvBunkerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MvBunkerApplication.class, args);
     }
--- MVBunker/src/main/resources/static/js/review/reviewDetail.js
+++ MVBunker/src/main/resources/static/js/review/reviewDetail.js
@@ -15,3 +15,18 @@ const tag = document.createElement('script');
 tag.src = "https://www.youtube.com/player_api";
 const firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+// 인피니티 스크롤링 설정
+infinityScrolling.url = ``; // 요청보낼 url
+infinityScrolling.makeItemList = makeItemList;
+infinityScrolling.setScrollInfinity(window);
+infinityScrolling.getItemList(1);
+function makeItemList(reviewList) { // 받은 애들 어떻게 뿌릴지
+    console.log(reviewList);
+    reviewList.forEach(review => {
+
+
+       // 지민이가 만든 컨테이너 주소 변수.innerHTML += `<div>${review.title}</div>`;
+    })
+}
