0118f9c31a56b3f7873b35dc0c5f55db0ef1a4b7
BigDog93 <ljc9393@nate.com>
2021-08-07 17:05:31 +0900

    리뷰매퍼

--- MVBunker/src/main/java/com/nurim/mvbunker/common/security/SecurityConfig.java
+++ MVBunker/src/main/java/com/nurim/mvbunker/common/security/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         security.authorizeRequests() // 로그인 없이 갈 수 있는 곳
 //                .antMatchers("/user/login", "/user/join", "/user/auth", "/movies/boxoffice")
-                .antMatchers("/user/**", "/movies/**", "/review/**", "/home") // test 용
+                .antMatchers("/**") // test 용
                 .permitAll()
                 .anyRequest().authenticated();
 
