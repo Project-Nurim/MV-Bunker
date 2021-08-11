7ff1ab3dc25a21e898ab183bdff39d569bbbea34
BigDog93 <ljc9393@nate.com>
2021-08-11 12:11:53 +0900

    인피니티 스크롤링을 위한 매퍼 LIMIT 인덱스 작업

--- MVBunker/src/main/java/com/nurim/mvbunker/common/model/PagingDTO.java
+++ MVBunker/src/main/java/com/nurim/mvbunker/common/model/PagingDTO.java
@@ -7,23 +7,19 @@ public class PagingDTO {
     private int page;
     private int listLength = 10;
     private int minIndex;
-    private int maxIndex;
     private int orderby;
     public PagingDTO(int page) {
         this.page = page;
-        this.minIndex = page * listLength - listLength;
-        this.maxIndex = page * listLength - 1;
+        this.minIndex = (page - 1) * listLength;
     }
     public PagingDTO(int page, int orderby) {
         this.page = page;
         this.orderby = orderby;
         this.minIndex = page * listLength - listLength;
-        this.maxIndex = page * listLength - 1;
     }
     public PagingDTO(int page, int orderby, int listLength) {
         this.page = page;
         this.orderby = orderby;
         this.minIndex = page * listLength - listLength;
-        this.maxIndex = page * listLength - 1;
     }
 }
