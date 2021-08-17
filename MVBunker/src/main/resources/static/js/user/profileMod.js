99b42a89d6fdceca683ca4a8a972b568fcef085f
BigDog93 <ljc9393@nate.com>
2021-08-18 04:03:28 +0900

    회원 탈퇴

--- MVBunker/src/main/resources/static/js/user/profileMod.js
+++ MVBunker/src/main/resources/static/js/user/profileMod.js
@@ -1,7 +1,18 @@
+document.querySelector('.trigger').addEventListener('click', (e) => {
+    document.querySelector('.modal-wrapper').classList.add('open');
+});
+document.querySelector('.btn-close').addEventListener('click', (e) => {
+    document.querySelector('.modal-wrapper').classList.remove('open');
+})
+
 function bye_confirm () {
-        if(confirm('탈퇴하시겠습니까?')){
-            alert("확인");
-        } else {
-            alert("취소");
-        }
+    const upw = document.getElementsByName('upw')[0].value;
+    const upw_chk = document.getElementsByName('upw_chk')[0].value;
+    const del_chk = document.getElementsByName('del_chk')[0].value;
+    if(upw === upw_chk && del_chk === '탈퇴하겠습니다.') {
+        return confirm('탈퇴하시겠습니까?');
+    }else {
+        alert('다시 입력해주십시오.');
+        return false;
     }
+}
