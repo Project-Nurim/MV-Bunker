913b2aaaa3eb8808c3406834fb31060ca12ba336
Ey <dmss3@naver.com>
2021-09-16 22:45:50 +0900

    PaingDTO >> minIndex의미가 이거였다

--- MVBunker/src/main/java/com/nurim/mvbunker/common/model/PagingDTO.java
+++ MVBunker/src/main/java/com/nurim/mvbunker/common/model/PagingDTO.java
@@ -6,7 +6,7 @@ import lombok.Data;
 public class PagingDTO {
     private int page;
     private int listLength = 10;
-    private int minIndex;
+    private int minIndex; //몇 번째 레코드부터 시작하는지! 시작하는 위치의 인덱스 값
     private int orderby;
     private String searchText;
     public PagingDTO(int page) {
