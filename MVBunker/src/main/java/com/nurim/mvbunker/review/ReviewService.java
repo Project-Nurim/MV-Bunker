113fd444001af31bb194c2da2a4a9ee367790432
BigDog93 <ljc9393@nate.com>
2021-10-08 14:18:06 +0900

    리뷰디테일 리뷰 불러오는거 기초작업

--- MVBunker/src/main/java/com/nurim/mvbunker/review/ReviewService.java
+++ MVBunker/src/main/java/com/nurim/mvbunker/review/ReviewService.java
@@ -34,8 +34,9 @@ public class ReviewService {
     }
 
     //특정 영화에 달린 리뷰
-    public List<ReviewDomain> getReviews(int id) {
-        return mapper.selReview(id);
+    public List<ReviewDomain> getReviews(ReviewEntity param) {
+        PagingDTO pagingDTO = new PagingDTO();
+        return mapper.selReview(param, pagingDTO);
     }
 
     // Evaluate CRUD
